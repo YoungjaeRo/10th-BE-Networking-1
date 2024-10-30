6ff92854c0898ac8a6806037a16b76ac9a2975ed
YoungjaeRo <ryj0330@naver.com>
2024-10-30 19:58:59 +0900

    feat: BackstagePassItemHandler 품질 증가 로직 최적화

--- Java/src/main/java/com/gildedrose/ItemHandler/BackstagePassItemHandler.java
+++ Java/src/main/java/com/gildedrose/ItemHandler/BackstagePassItemHandler.java
@@ -7,17 +7,22 @@ public class BackstagePassItemHandler implements ItemHandler {
     @Override
     public void handleItem(Item item) {
         item.sellIn -= 1;
-        if (item.quality < 50) {
-            item.quality += 1;
-        }
-        if (item.sellIn <= 10 && item.quality < 50) {
-            item.quality += 1;
-        }
-        if (item.sellIn <= 5 && item.quality < 50) {
-            item.quality += 1;
-        }
+
         if (item.sellIn < 0) {
             item.quality = 0;
+            return;
+        }
+
+        if (item.quality < GlidedRose.MAX_QUALITY) {
+            item.quality += 1;
+
+            if (item.sellIn < 10) {
+                item.quality += 1;
+            }
+
+            if (item.sellIn < 5) {
+                item.quality += 1;
+            }
         }
     }
 }
